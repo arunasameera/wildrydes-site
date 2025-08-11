ea01f9f27fcb2af1ca7e0ac067956378bac51888
arunasameera <asliyanage@gmail.com>
2025-08-11 13:24:58 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_kXPhtqDmT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1550ic3m6l62h0jdfu43ue9avh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3r0rrognmed9ubhld6nlgglpmi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
