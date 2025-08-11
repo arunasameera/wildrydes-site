6a16b10976526cea3f401a1563d0025470c37adf
arunasameera <asliyanage@gmail.com>
2025-08-11 13:12:35 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_kXPhtqDmT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1550ic3m6l62h0jdfu43ue9avh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
