7548252733ba2532cec1bee6ec0bf1fef9caaa49
arunasameera <asliyanage@gmail.com>
2025-08-11 13:13:24 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_kXPhtqDmT', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1550ic3m6l62h0jdfu43ue9avh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
